239594610880af0c42e098692616d6a098ab82e0
asirko <sirko.alexandre@gmail.com>
2023-08-18 19:09:09 +0200

    Auto-generated commit [ci skip]

---
+++ app/features/01-svg-example/basic-example/basic-example.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lcd-basic-example',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './basic-example.component.html',
+  styleUrls: ['./basic-example.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  preserveWhitespaces: true,
+})
+export class BasicExampleComponent {}
