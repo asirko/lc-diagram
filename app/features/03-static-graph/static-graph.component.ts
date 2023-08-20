2c880ffe571fd5cf5e0f4c8f834112150b2e52a9
asirko <sirko.alexandre@gmail.com>
2023-08-20 13:12:33 +0200

    Auto-generated commit [ci skip]

--- app/features/03-static-graph/static-graph.component.ts
+++ app/features/03-static-graph/static-graph.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CodeReaderComponent } from '../../shared/components/code-reader/code-reader.component';
+import { ParentComponent } from '../02-component-svg/parent/parent.component';
+import { StaticDiagramComponent } from './static-diagram/static-diagram.component';
 
 @Component({
   selector: 'lcd-static-graph',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CodeReaderComponent, ParentComponent, StaticDiagramComponent],
   templateUrl: './static-graph.component.html',
   styleUrls: ['./static-graph.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
